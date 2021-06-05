8fd3125458b4d8aaa9d7768101af35df1459f9bd
sunghyoeun <shyoeuny@gmail.com>
2021-06-05 23:06:26 +0900

    upgrade

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,67 @@
             숙박 설비를 아무것도 갖지 않는 야영 등도 넓은 의미로는 포함하고 있다
           </div>
         </article>
+        <article class = "notification">
+          <div class="message-header">
+            <p>등산을 동반하는 캠핑 필수품</p>
+          </div>
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr >
+                <th>기본물품</th>
+                <th>구급약</th>
+                <th>구난용품</th>
+                <th>식량</th>
+                <th>식수</th>
+                <th>옷</th>
+                <th>편의물품</th>
+                <th>해충구제</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class = "is-selected">
+                <td>배낭</td>
+                <td>반창고</td>
+                <td>나침반</td>
+                <td>비상식량</td>
+                <td>생수</td>
+                <td>고글</td>
+                <td>물티슈</td>
+                <td>모기장</td>
+              </tr>
+              <tr>
+                <td>수건</td>
+                <td>붕대</td>
+                <td>라디오</td>
+                <td>전투식량</td>
+                <td>정수제</td>
+                <td>등산화</td>
+                <td>숟가락</td>
+                <td>살충제</td>
+              </tr>
+              <tr>
+                <td>수통</td>
+                <td>소독약</td>
+                <td>로프</td>
+                <td>통조림</td>
+                <td>차</td>
+                <td>깔깔이</td>
+                <td>야전샵</td>
+                <td>해충기피제</td>
+              </tr>
+              <tr>
+                <td>지팡이</td>
+                <td>소화제</td>
+                <td>멀티툴 <small>(맥가이버 칼)</small></td>
+                <td></td>
+                <td>필터</td>
+                <td>모자</td>
+                <td>자외선 차단제</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
       </div>
       <div class="column">
         <article class="message">
@@ -39,6 +100,33 @@
             </figure>
             오토캠핑, 백패킹, 오지 캠핑,가족캠핑,솔로캠핑,팀캠핑,미니멀 캠핑,감성 캠핑,원정 캠핑,비박,글램핑,도시 캠핑,옥상 캠핑
           </div>
+        </article>
+        <article class ="notification">
+          <div class="message-header">
+            <p>캠핑에 필요없는 물품</p>
+          </div>
+          <table class = "table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>물품 이름</th>
+                <th>설명</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class = "is-selected">
+                <td>마체테</td>
+                <td>마체테를 이용해 가지를 치거나 호신용으로 써야 하는 곳은 애초에 입산금지 지역이다.</td>
+              </tr>
+              <tr>
+                <td>무전기</td>
+                <td>스마트폰이 더 편하고 잘 터지며 기능도 더 많다. </td>
+              </tr>
+              <tr>
+                <td>세탁비누</td>
+                <td>캠핑장에서의 세탁은 번거로운 것은 물론, 경우에 따라서는 환경을 오염시킬 수 있다. </td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
     </div>
