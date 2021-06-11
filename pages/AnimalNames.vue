f6751a8de98b78e281dc941fcac0c7d15cf7fb75
sunghyoeun <shyoeuny@gmail.com>
2021-06-11 19:27:58 +0900

    update

--- pages/AnimalNames.vue
+++ pages/AnimalNames.vue
@@ -10,7 +10,7 @@
                 </p>
             </div>
         </section>
-        <hr/>
+
         <section class = "columns">
             <div class="column">
                 <table class = "table">
@@ -36,19 +36,55 @@
                     
                 </div>
             </div>
-        </section>
 
+            <div class="column">
+                <table class = "table">
+                    <thead><div class="message-header"><p>안전사고</p></div>
+                        <tr>
+                            <th>번호</th>
+                            <th>사고</th>
+                            <th>사고 건수</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <template v-for=" pos in tableAccidentsKey.length">
+                            <tr :key="pos">
+                                <td>{{pos}}</td>
+                                <td>{{tableAccidentsKey [pos - 1]}}</td>
+                                <td>
+                                    <span class="tag is-black" v-if="((ar = tableAccidents[tableAccidentsKey[pos-1]]), ar.length == 0)">없음</span>
+                                    <span v-else><template v-for="subbread in ar"><span class="tag is-success" :key="subbread">{{subbread}}</span>&nbsp;</template></span>
+                                </td>
+                            </tr>
+                        </template>
+                    </tbody>
+                </table>
+                <div class="content">
+                    <p class = "tag is-danger">출처</p>
+                    <a href="https://gist.github.com/">github</a><br>
+                    <a class = "button is-primary is-small" href="https://github.com/sunghyoeun/doglove3_2021/blob/master/static/warning.json">accident.json</a>
+                </div>
+                <div class="column">
+                       
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 <script>
 	import axios from 'axios';
+    import axois2 from 'axios';
+
 	export default {
 		async asyncData() {
 			const AnimalNames = await axios.get('https://gist.githubusercontent.com/borlaym/585e2e09dd6abd9b0d0a/raw/6e46db8f5c27cb18fd1dfa50c7c921a0fbacbad0/animals.json');
+            const Accidents = await axois2.get('https://raw.githubusercontent.com/sunghyoeun/doglove3_2021/master/static/warning.json');
             //alert(Object.keys(AnimalNames));
             return {
                 tableDogBreeds: AnimalNames.data,
+                tableAccidents : Accidents.data.message,
+                tableAccidentsKey : Object.keys(Accidents.data.message),
             };
-		}
+		},
 	};
 </script>
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "add json"
+git commit -m "update"
 git push -u origin master
